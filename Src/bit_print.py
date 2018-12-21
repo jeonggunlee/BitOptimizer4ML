73c8ad3bcb6ecb599c6cc88336f006de76827a28
Jeong-Gun Lee <jeonggun.lee@gmail.com>
2018-12-21 19:44:06 +0900

    Update bit_print.py

--- Src/bit_print.py
+++ Src/bit_print.py
@@ -1,9 +1,13 @@
+# Test Code for binRep: It show how the an 32 bit floating point number is represented in a binary string.
+
 import tensorflow as tf
 import random
 import numpy as np
 import ctypes
 
-
+# The following code comes from:
+# https://stackoverflow.com/questions/16444726/binary-representation-of-float-in-python-bits-not-hex
+#
 def binRep(num):
         binNum = bin(ctypes.c_uint.from_buffer(ctypes.c_float(num)).value)[2:]
         print("bits: " + binNum.rjust(32,"0"))
@@ -17,9 +21,8 @@ def binRep(num):
         print("sign:" + str(sign))
         print("recreate:" + str(sign*mantInt*(2**base)))
 
-
-
-
+        
+# Test...
 binRep(0.01171875)
 binRep(0.013671875)
 binRep(0.015625)
